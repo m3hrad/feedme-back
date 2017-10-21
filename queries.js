234b3e6b1d7cc956174d90f19ebe89e0ee11d1e2
m3hrad <mehrad.mohammadi@futurice.com>
2017-10-22 01:30:28 +0300

    the getAllRecipes end point added, the sql script added.

---
+++ queries.js
@@ -0,0 +1,32 @@
+var promise = require('bluebird');
+
+var options = {
+    // Initialization Options
+    promiseLib: promise
+};
+
+var pgp = require('pg-promise')(options);
+var connectionString = 'postgres://feedme:feedme@localhost:5432/feedme';
+var db = pgp(connectionString);
+
+// add query functions
+
+function getAllRecipes(req, res, next) {
+    db.any('select * from recipes')
+        .then(function (data) {
+            res.status(200)
+                .json({
+                    status: 'success',
+                    data: data,
+                    message: 'Retrieved ALL recipes'
+                });
+        })
+        .catch(function (err) {
+            return next(err);
+        });
+}
+
+
+module.exports = {
+    getAllRecipes: getAllRecipes,
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+var db = require('../queries');
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/api/recipes', db.getAllRecipes);
+// router.get('/api/recipes/:id', db.getSingleRecipes);
+// router.post('/api/recipes', db.createRecipe);
+// router.put('/api/recipes/:id', db.updateRecipe);
+// router.delete('/api/recipes/:id', db.removeRecipe);
+//
+// router.get('/api/ingredients', db.getAllIngredients);
+// router.get('/api/ingredients/:id', db.getSingleIngredient);
+// router.post('/api/ingredients', db.createIngredient);
+// router.put('/api/ingredients/:id', db.updateIngredient);
+// router.delete('/api/ingredients/:id', db.removeIngredient);
 
 module.exports = router;
